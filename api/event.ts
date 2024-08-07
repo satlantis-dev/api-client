aec0631044242019bcc3772b912d63fdad1ec1d2
BlowaterNostr <blowater.nostr@proton.me>
2024-08-07 07:08:33 +0000

    tutorial video

--- api/event.ts
+++ api/event.ts
@@ -14,16 +14,3 @@ async (args: {
     }
     return handleResponse<PlaceEvent>(response);
 };
-
-export const this_is_a_new_API = (urlArg: URL) =>
-    async (args: {
-        placeID: number;
-    }) => {
-        const url = copyURL(urlArg);
-        url.pathname = `/this_is_a_new_API/${args.placeID}`;
-        const response = await safeFetch(url);
-        if (response instanceof Error) {
-            return response;
-        }
-        return handleResponse<PlaceEvent>(response);
-    };
