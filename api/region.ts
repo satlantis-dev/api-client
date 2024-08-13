f1b3168a4ae2bdebe87c8a152d7146636b0af686
BlowaterNostr <blowater.nostr@proton.me>
2024-08-13 04:50:51 +0000

    fix

---
+++ api/region.ts
@@ -0,0 +1,55 @@
+import { Category, Country, NostrEvent, OSMType, Place, Topic } from "../sdk.ts";
+import { Metric } from "./metric.ts";
+
+export type RegionMetric = {
+    dataPoints: number;
+    regionId: number;
+    metricId: number;
+    metric: Metric;
+    updatedAt: Date;
+    value: number;
+    valueStr: string;
+    score: number;
+};
+
+export interface Region {
+    id: number;
+    code: string;
+    banner: string;
+    categoryScores: RegionCategoryScore[];
+    countryId: number;
+    country: Country;
+    description: string;
+    eventId: number;
+    event: NostrEvent;
+    lat: number;
+    lng: number;
+    metrics: RegionMetric[];
+    name: string;
+    osmId: number;
+    osmLevel: string;
+    osmType: OSMType;
+    osmRef: string;
+    places: Place[];
+    slug: string;
+    hashtags: string[];
+}
+
+export interface RegionCategoryScore {
+    categoryId: number;
+    category: Category;
+    regionId: number;
+    score: number;
+    topicScores: RegionTopicScore[];
+    updatedAt: Date;
+}
+
+export interface RegionTopicScore {
+    categoryId: number;
+    regionId: number;
+    score: number;
+    topicId: number;
+    topic: Topic;
+    userNumber: number;
+    userScore: number;
+}
