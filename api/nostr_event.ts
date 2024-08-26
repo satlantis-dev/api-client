b63e14d05464331b1a58a0eac5e659f3bb67d3e7
BlowaterNostr <blowater.nostr@proton.me>
2024-08-26 10:38:15 +0000

    +

--- api/nostr_event.ts
+++ api/nostr_event.ts
@@ -1,35 +1,36 @@
-import type { NostrEvent, UnsignedNostrEvent } from "@blowater/nostr-sdk";
+import type { NostrEvent, NostrKind, UnsignedNostrEvent } from "@blowater/nostr-sdk";
 import { ApiError, copyURL, handleResponse } from "../helpers/_helper.ts";
 import { safeFetch } from "../helpers/safe-fetch.ts";
 import type { func_GetJwt } from "../sdk.ts";
 
-export const signEvent = (urlArg: URL, getJwt: func_GetJwt) => async (args: UnsignedNostrEvent) => {
-    const url = copyURL(urlArg);
-    url.pathname = `/secure/signEvent`;
-    const response = await safeFetch(url, {
-        method: "POST",
-        body: JSON.stringify(args),
-        headers: new Headers({
-            "Authorization": `Bearer ${getJwt()}`,
-        }),
-    });
-    if (response instanceof Error) {
-        return response;
-    }
-    const res = await handleResponse<NostrEvent>(response);
-    if (res instanceof ApiError) {
-        if (res.status == 401) {
-            return {
-                type: 401 as 401,
-                data: res.message,
-            };
+export const signEvent =
+    (urlArg: URL, getJwt: func_GetJwt) => async <K extends NostrKind>(args: UnsignedNostrEvent<K>) => {
+        const url = copyURL(urlArg);
+        url.pathname = `/secure/signEvent`;
+        const response = await safeFetch(url, {
+            method: "POST",
+            body: JSON.stringify(args),
+            headers: new Headers({
+                "Authorization": `Bearer ${getJwt()}`,
+            }),
+        });
+        if (response instanceof Error) {
+            return response;
         }
-    }
-    if (res instanceof Error) {
-        return res;
-    }
-    return {
-        type: true as true,
-        data: res,
+        const res = await handleResponse<NostrEvent<K>>(response);
+        if (res instanceof ApiError) {
+            if (res.status == 401) {
+                return {
+                    type: 401 as 401,
+                    data: res.message,
+                };
+            }
+        }
+        if (res instanceof Error) {
+            return res;
+        }
+        return {
+            type: true as true,
+            data: res,
+        };
     };
-};
