87ba4675f6801059b9e4c3e50a8e4df206142759
BlowaterNostr <blowater.nostr@proton.me>
2024-09-06 07:27:05 +0000

    export type

---
+++ models/interest.ts
@@ -0,0 +1,10 @@
+export type Interest = {
+    readonly id: number;
+    readonly name: string;
+    readonly description: string;
+    readonly recommendationsByNpub: string[] | null | undefined;
+    readonly recommendationsById: null | undefined;
+    readonly autofollowsByNpub: null | undefined;
+    readonly autofollowsById: null | undefined;
+    readonly hashtags: string[] | null | undefined;
+};
