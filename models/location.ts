a5373fe6fb81737478fc175d5f19f1f907282acc
SatoshiSound <satoshisound@protonmail.com>
2024-09-28 14:20:16 +0100

    fix formatting

--- models/location.ts
+++ models/location.ts
@@ -1,71 +1,64 @@
-import type {
-	BusinessStatus,
-	Note,
-	NoteType,
-	OpeningHours,
-	PriceLevel,
-	ReshapedNostrEvent
-} from '../sdk.ts';
+import type { BusinessStatus, Note, NoteType, OpeningHours, PriceLevel, ReshapedNostrEvent } from "../sdk.ts";
 
 export type Location = {
-	id: number;
-	name: string;
-	accounts: LocationAccount[];
-	image: string;
-	lat: number;
-	lng: number;
-	locationTags: LocationTag[];
-	osmRef: string;
-	googleId: string;
-	placeId: number;
-	eventId: number;
-	event: ReshapedNostrEvent;
-	score: number;
-	address: Address;
-	phone: string;
-	notes: LocationNote[];
-	businessStatus: BusinessStatus;
-	openingHours: OpeningHours;
-	priceLevel: PriceLevel;
-	googleRating: number;
-	googleUserRatingCount: number;
-	websiteUrl: string;
+    id: number;
+    name: string;
+    accounts: LocationAccount[];
+    image: string;
+    lat: number;
+    lng: number;
+    locationTags: LocationTag[];
+    osmRef: string;
+    googleId: string;
+    placeId: number;
+    eventId: number;
+    event: ReshapedNostrEvent;
+    score: number;
+    address: Address;
+    phone: string;
+    notes: LocationNote[];
+    businessStatus: BusinessStatus;
+    openingHours: OpeningHours;
+    priceLevel: PriceLevel;
+    googleRating: number;
+    googleUserRatingCount: number;
+    websiteUrl: string;
 };
 
 export interface Address {
-	streetNumber: string;
-	route: string;
-	locality: string;
-	postalCode: string;
-	country: string;
+    streetNumber: string;
+    route: string;
+    locality: string;
+    postalCode: string;
+    country: string;
 }
 
 export interface LocationNote {
-	id: number;
-	locationId: number;
-	noteId: number;
-	note: Note;
-	type: NoteType;
+    id: number;
+    locationId: number;
+    noteId: number;
+    note: Note;
+    type: NoteType;
 }
 
 export interface LocationTag {
-	id: number;
-	category: string;
-	key: string;
-	value: string;
-	eligible: boolean;
-	locations: Location[] | null | undefined;
+    id: number;
+    category: string;
+    key: string;
+    value: string;
+    eligible: boolean;
+    locations: Location[] | null | undefined;
 }
 
 export enum LocationAccountTypeEnum {
-	OWNER = 'owner',
-	MANAGER = 'manager',
-	MEMBER = 'member',
-	VISITOR = 'visitor'
+    OWNER = "owner",
+    MANAGER = "manager",
+    MEMBER = "member",
+    VISITOR = "visitor",
 }
 
 export interface LocationAccount {
-	locationId: number;
-	accountId: number;
-	type: LocationAccountTypeEnum;
+    locationId: number;
+    accountId: number;
+    type: LocationAccountTypeEnum;
 }
