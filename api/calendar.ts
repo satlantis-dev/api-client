3abf1ea335aef58de81b4be53f3af50c6a02ff01
SatoshiSound <satoshisound@protonmail.com>
2024-10-03 17:22:04 +0100

    format

--- api/calendar.ts
+++ api/calendar.ts
@@ -1,30 +1,30 @@
-import { CalendarEventType } from '../models/calendar.ts';
+import { CalendarEventType } from "../models/calendar.ts";
 
 export const getEventTypeUsingName = (id: string): CalendarEventType => {
-	switch (id) {
-		case 'Conference':
-			return CalendarEventType.Conference;
-		case 'Meetup':
-			return CalendarEventType.Meetup;
-		case 'Hackathon':
-			return CalendarEventType.Hackathon;
-		case 'Concert':
-			return CalendarEventType.Concert;
-		case 'Workshop':
-			return CalendarEventType.Workshop;
-		case 'Party':
-			return CalendarEventType.Party;
-		case 'Play':
-			return CalendarEventType.Play;
-		case 'Sports':
-			return CalendarEventType.Sports;
-		case 'Exhibition':
-			return CalendarEventType.Exhibition;
-		case 'Festival':
-			return CalendarEventType.Festival;
-		case 'Music':
-			return CalendarEventType.Music;
-		default:
-			return CalendarEventType.Other;
-	}
+    switch (id) {
+        case "Conference":
+            return CalendarEventType.Conference;
+        case "Meetup":
+            return CalendarEventType.Meetup;
+        case "Hackathon":
+            return CalendarEventType.Hackathon;
+        case "Concert":
+            return CalendarEventType.Concert;
+        case "Workshop":
+            return CalendarEventType.Workshop;
+        case "Party":
+            return CalendarEventType.Party;
+        case "Play":
+            return CalendarEventType.Play;
+        case "Sports":
+            return CalendarEventType.Sports;
+        case "Exhibition":
+            return CalendarEventType.Exhibition;
+        case "Festival":
+            return CalendarEventType.Festival;
+        case "Music":
+            return CalendarEventType.Music;
+        default:
+            return CalendarEventType.Other;
+    }
 };
--- models/calendar.ts
+++ models/calendar.ts
@@ -1,78 +1,78 @@
-import type { Note, ReshapedNostrEvent } from '../sdk.ts';
+import type { Note, ReshapedNostrEvent } from "../sdk.ts";
 
-import type { Account } from './account.ts';
+import type { Account } from "./account.ts";
 
 export enum CalendarEventType {
-	Conference = 'conference',
-	Meetup = 'meetup',
-	Hackathon = 'hackathon',
-	Concert = 'concert',
-	Workshop = 'workshop',
-	Party = 'party',
-	Play = 'play',
-	Sports = 'sports',
-	Exhibition = 'exhibition',
-	Festival = 'festival',
-	Music = 'music',
-	Other = 'other'
+    Conference = "conference",
+    Meetup = "meetup",
+    Hackathon = "hackathon",
+    Concert = "concert",
+    Workshop = "workshop",
+    Party = "party",
+    Play = "play",
+    Sports = "sports",
+    Exhibition = "exhibition",
+    Festival = "festival",
+    Music = "music",
+    Other = "other",
 }
 
 export interface CalendarEventRSVP {
-	id: number;
-	accountId: number;
-	account: Account;
-	createdAt: string;
-	content: string;
-	eventId: number;
-	nostrId: string;
-	pubkey: string;
-	sig: string;
-	tags: string;
-	calendarEventId: number;
-	status: string;
+    id: number;
+    accountId: number;
+    account: Account;
+    createdAt: string;
+    content: string;
+    eventId: number;
+    nostrId: string;
+    pubkey: string;
+    sig: string;
+    tags: string;
+    calendarEventId: number;
+    status: string;
 }
 
 export interface CalendarEventNote {
-	id: number;
-	calendarEventId: number;
-	noteId: number;
-	note: Note;
+    id: number;
+    calendarEventId: number;
+    noteId: number;
+    note: Note;
 }
 
 export type CalendarEvent = {
-	id: number;
-	atag: string;
-	accountId: number;
-	account: Account;
-	announcements: CalendarEventNote[];
-	calendarEventRsvps: CalendarEventRSVP[];
-	content: string;
-	createdAt: string;
-	dtag: string;
-	end?: Date;
-	endTzId?: string;
-	eventId: number;
-	geohash?: string;
-	image: string;
-	kind: number;
-	location?: string;
-	nostrId: string;
-	notes: CalendarEventNote[];
-	pubkey: string;
-	sig: string;
-	start: Date;
-	startTzId?: string;
-	summary: string;
-	tags: string;
-	title: string;
-	type: CalendarEventType;
-	url: string;
+    id: number;
+    atag: string;
+    accountId: number;
+    account: Account;
+    announcements: CalendarEventNote[];
+    calendarEventRsvps: CalendarEventRSVP[];
+    content: string;
+    createdAt: string;
+    dtag: string;
+    end?: Date;
+    endTzId?: string;
+    eventId: number;
+    geohash?: string;
+    image: string;
+    kind: number;
+    location?: string;
+    nostrId: string;
+    notes: CalendarEventNote[];
+    pubkey: string;
+    sig: string;
+    start: Date;
+    startTzId?: string;
+    summary: string;
+    tags: string;
+    title: string;
+    type: CalendarEventType;
+    url: string;
 };
 
 export type PlaceCalendarEvent = {
-	id: number;
-	createdAt: string;
-	placeId: number;
-	calendarEventId: number;
-	calendarEvent: CalendarEvent;
+    id: number;
+    createdAt: string;
+    placeId: number;
+    calendarEventId: number;
+    calendarEvent: CalendarEvent;
 };
