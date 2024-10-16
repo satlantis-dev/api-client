54a5cb57d6426b61dfd27bb026e97f6c771b110e
alberlantis <albert@satlantis.io>
2024-10-16 08:42:26 +0000

    get reactions of a note

--- resolvers/note.ts
+++ resolvers/note.ts
@@ -1,4 +1,4 @@
-import type { NostrEvent } from "@blowater/nostr-sdk";
+import { type NostrEvent, NostrKind, SingleRelayConnection } from "@blowater/nostr-sdk";
 import type { Client, Note } from "../sdk.ts";
 
 export class NoteResolver {
@@ -39,5 +39,49 @@ export class NoteResolver {
             return user;
         }
         return user;
+    };
+
+    /**
+     * get reactions (kind 7) of this note
+     * @experimental
+     * @unstable parameters are subjects to changes.
+     *  implementations might change
+     */
+    getReactions = async (args: {
+        limit: number;
+        since: Date;
+    }) => {
+        const reactions = [];
+        {
+            const relay = SingleRelayConnection.New(this.client.relay_url, { log: false });
+            if (relay instanceof Error) {
+                return relay;
+            }
+            const stream = await relay.newSub("getReactions", {
+                "#e": [this.source.type == "nostr" ? this.source.data.id : this.source.data.nostrId],
+                kinds: [NostrKind.REACTION],
+                limit: args.limit,
+                since: Math.floor(args.since.valueOf() / 1000),
+            });
+            if (stream instanceof Error) {
+                await relay.close();
+                return stream;
+            }
+            for await (const msg of stream.chan) {
+                if (msg.type == "EOSE") {
+                    break;
+                } else if (msg.type == "EVENT") {
+                    const note = new NoteResolver(this.client, {
+                        type: "nostr",
+                        data: msg.event,
+                    });
+                    reactions.push(note);
+                } else if (msg.type == "NOTICE") {
+                    console.warn(msg.note);
+                }
+            }
+            await relay.close();
+        }
+        return reactions;
     };
 }
