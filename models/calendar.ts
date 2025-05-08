f727b65d9817661d0ac54defe40b1991d9005e8b
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2025-05-09 03:17:13 +0400

    Main cohosts (#191)
    * feat: interfaces needed for sat-2226
    
    * feat: add cohosts and venue tags
    
    * chore: fmt
    
    * chore: new param added by vano
    
    ---------
    
    Co-authored-by: cavini <arthurcavini@hotmail.com>
    Co-authored-by: Bob <dv4xd@proton.me>

--- models/calendar.ts
+++ models/calendar.ts
@@ -1,87 +1,113 @@
+import type { Location, LocationResolver, Note, Place } from "../sdk.ts";
 import type { AccountDTO } from "./account.ts";
-import type { Note } from "../api/note.ts";
 
 export interface CalendarEventRSVP {
-    id: number;
-    accountId: number;
-    account: AccountDTO;
-    createdAt: string;
-    content: string;
-    eventId: number;
-    nostrId: string;
-    pubkey: string;
-    sig: string;
-    tags: string;
-    calendarEventId: number;
-    status: string;
+  id: number;
+  accountId: number;
+  account: AccountDTO;
+  createdAt: string;
+  content: string;
+  eventId: number;
+  nostrId: string;
+  pubkey: string;
+  sig: string;
+  tags: string;
+  calendarEventId: number;
+  status: string;
 }
 
 export interface CalendarEventAnnouncement {
-    id: number;
-    calendarEventId: number;
-    noteId: number;
-    note: Note;
+  id: number;
+  calendarEventId: number;
+  noteId: number;
+  note: Note;
 }
 
 export interface CalendarEventNote {
-    id: number;
-    calendarEventId: number;
-    noteId: number;
-    note: Note;
+  id: number;
+  calendarEventId: number;
+  noteId: number;
+  note: Note;
 }
 
+export type EventInterest = {
+  id: number;
+  name: string;
+};
+
+export type Interest = {
+  autofollowsById: null | string;
+  autofollowsByNpub: null | string;
+  category: number;
+  contentUse: boolean;
+  description: string;
+  eventUse: boolean;
+  hashtags: null | string;
+  id: number;
+  locationTags: null | string;
+  locationUse: boolean;
+  name: string;
+  peopleUse: boolean;
+  recommendationsById: null | string;
+  recommendationsByNpub: null | string;
+  section: string;
+};
+export type Cohost = {
+  account: AccountDTO;
+  accountId: number;
+
+  calendarEventId: number;
+  createdAt: string;
+  id: number;
+  invitationAcceptedAt: string | null;
+  invitationDeclinedAt: string | null;
+  updatedAt: string;
+};
 export type CalendarEvent = {
-    id: number;
-    atag: string;
-    accountId: number;
-    account: AccountDTO;
-    cohosts: {
-        account: AccountDTO;
-        accountId: number;
-        calendarEventId: number;
-        createdAt: string;
-        id: number;
-        invitationAcceptedAt: string | null;
-        invitationDeclinedAt: string | null;
-        updatedAt: string;
-    }[];
-    announcements: CalendarEventAnnouncement[];
-    calendarEventRsvps: CalendarEventRSVP[];
-    content: string;
-    createdAt: string;
-    dtag: string;
-    end: string;
-    endTzId?: string;
-    eventId: number;
-    geohash?: string;
-    image: string;
-    kind: number;
-    location?: string;
-    nostrId: string;
-    notes: CalendarEventNote[];
-    pubkey: string;
-    sig: string;
-    start: string;
-    startTzId?: string;
-    summary: string;
-    tags: string;
-    title: string;
-    venue?: Location;
-    type: CalendarEventType;
-    url: string;
-    website: string;
+  id: number;
+  atag: string;
+  placeId: number;
+  interests: Interest[];
+  accountId: number;
+  account: AccountDTO;
+  cohosts: Cohost[];
+  announcements: CalendarEventAnnouncement[];
+  calendarEventRsvps: CalendarEventRSVP[];
+  content: string;
+  createdAt: string;
+  dtag: string;
+  end: string;
+  endTzId?: string;
+  eventId: number;
+  geohash?: string;
+  image: string;
+  kind: number;
+  location?: string;
+  nostrId: string;
+  notes: CalendarEventNote[];
+  pubkey: string;
+  sig: string;
+  start: string;
+  startTzId?: string;
+  summary: string;
+  tags: string;
+  title: string;
+  venue?: Location;
+  type: CalendarEventType;
+  url: string;
+  website: string;
 };
 
 export type PlaceCalendarEvent = {
-    id: number;
-    createdAt: string;
-    placeId: number;
-    calendarEventId: number;
-    calendarEvent: CalendarEvent;
+  id: number;
+  createdAt: string;
+  placeId: number;
+  calendarEventId: number;
+  calendarEvent: CalendarEvent;
 };
 
 export type CalendarEventType = {
-    id: number;
-    name: string;
-    description: string;
+  id: number;
+  name: string;
+  description: string;
 };
