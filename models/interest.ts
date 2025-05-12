e4593c19777e0fdde7ec8895bc4c14f50d9393a7
cavini <60157060+cavini@users.noreply.github.com>
2025-05-12 20:23:14 -0300

    feat: add section to interest type (#195)

--- models/interest.ts
+++ models/interest.ts
@@ -2,26 +2,27 @@ import type { LocationTag } from "./location.ts";
 
 // https://github.com/satlantis-dev/models/blob/main/interest.go#L19
 export type Interest = {
-    readonly id: number;
-    readonly name: string;
-    readonly description: string;
-    readonly recommendationsByNpub: string[] | null | undefined;
-    readonly recommendationsById: null | undefined;
-    readonly autofollowsByNpub: string[] | null | undefined;
-    readonly autofollowsById: null | undefined;
-    readonly hashtags: string[] | null | undefined;
-    readonly locationTags: LocationTag[];
-    readonly category: InterestCategory;
-    readonly contentUse: boolean;
-    readonly locationUse: boolean;
-    readonly eventUse: boolean;
-    readonly peopleUse: boolean;
+  readonly id: number;
+  readonly name: string;
+  readonly description: string;
+  readonly recommendationsByNpub: string[] | null | undefined;
+  readonly recommendationsById: null | undefined;
+  readonly autofollowsByNpub: string[] | null | undefined;
+  readonly autofollowsById: null | undefined;
+  readonly hashtags: string[] | null | undefined;
+  readonly locationTags: LocationTag[];
+  readonly category: InterestCategory;
+  readonly contentUse: boolean;
+  readonly locationUse: boolean;
+  readonly eventUse: boolean;
+  readonly peopleUse: boolean;
+  readonly section: string;
 };
 
 export enum InterestCategory {
-    GeneralInterest = 1,
-    LocationInterest,
-    ActivityInterest,
-    FoodInterest,
-    NicheInterest,
+  GeneralInterest = 1,
+  LocationInterest,
+  ActivityInterest,
+  FoodInterest,
+  NicheInterest,
 }
