889491fb392ea6a8eb1428c945dd1c242158a952
Bob <dv4xd@proton.me>
2025-09-03 20:03:25 +0800

    Update postCalendarEventRSVP to allow boolean answers in registrationAnswers

--- api/secure/calendar.ts
+++ api/secure/calendar.ts
@@ -279,7 +279,7 @@ export const postCalendarEventRSVP =
         registrationAnswers?: {
             label: string;
             answerType: AnswerType;
-            answer: string;
+            answer: string | boolean;
         }[];
     }) => {
         const jwtToken = getJwt();
