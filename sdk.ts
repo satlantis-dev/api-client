39139148f2ab786a015a59ff3b21815dbd306368
Bob <dv4xd@proton.me>
2025-11-05 20:06:52 +0800

    feat: add importEventFromUrl API to import events from a given URL

--- sdk.ts
+++ sdk.ts
@@ -130,6 +130,7 @@ import {
     getCalendarsByAccount,
     getEventById,
     getEventSubmissions,
+    importEventFromUrl,
     postCalendarEventAnnouncement,
     postCalendarEventNote,
     postCalendarEventRSVP,
@@ -329,6 +330,7 @@ export class Client {
     approveEventSubmission: ReturnType<typeof approveEventSubmission>;
     declineEventSubmission: ReturnType<typeof declineEventSubmission>;
     getEventSubmissions: ReturnType<typeof getEventSubmissions>;
+    importEventFromUrl: ReturnType<typeof importEventFromUrl>;
 
     // Account
     /**
@@ -629,6 +631,7 @@ export class Client {
         this.approveEventSubmission = approveEventSubmission(rest_api_url, getJwt);
         this.declineEventSubmission = declineEventSubmission(rest_api_url, getJwt);
         this.getEventSubmissions = getEventSubmissions(rest_api_url, getJwt);
+        this.importEventFromUrl = importEventFromUrl(rest_api_url, getJwt);
 
         // account
         this._getAccount = getAccount(rest_api_url, getJwt);
