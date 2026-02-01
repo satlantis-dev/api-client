ccaf9ea0ee93e9fa06318e15af9e15f969c203e7
Jin Shin <jinshin1013@gmail.com>
2026-02-01 16:04:54 +0900

    Updated TicketTypeOrder type

--- api/events.ts
+++ api/events.ts
@@ -886,6 +886,8 @@ export interface EventTicketPurchasePayload {
         {
             ticketTypeId: number;
             quantity: number;
+            priceSats?: number;
+            priceFiat?: number;
         },
     ];
     rsvpData: {
